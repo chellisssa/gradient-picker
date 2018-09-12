1a684fa76b713628f60622ca0ef7cf6dd4627c24
chellisssa <chellisssa@gmail.com>
2018-09-13 01:40:53 +0300

    Added radial gradient and fixed code box

--- script.js
+++ script.js
@@ -9,26 +9,43 @@ var toBottomLeft = document.querySelector(".bottom-left");
 var toLeft = document.querySelector(".left");
 var toTopLeft = document.querySelector(".top-left");
 var output = document.querySelector(".output");
+var radial = document.querySelector(".radial");
+var body = document.querySelector("body");
+var radialCode = document.querySelector(".radial-code");
+var linearCode = document.querySelector(".linear-code");
 
 // DEFAULT VALUES
 document.getElementById("colorStart").innerHTML = "#ff80ff";
+document.getElementById("colorStartRad").innerHTML = "#ff80ff";
 document.getElementById("colorEnd").innerHTML = "#ffff80";
+document.getElementById("colorEndRad").innerHTML = "#ffff80";
 document.getElementById("direction").innerHTML = "to top right";
 
 
 function handleUpdate() {
   document.documentElement.style.setProperty(`--${this.name}`, this.value);
   if (`--${this.name}` === '--colorStart') {
-    document.getElementById("colorStart").innerHTML = this.value;
-  } else {
-    document.getElementById("colorEnd").innerHTML = this.value;
+    document.getElementById("colorStart").textContent = this.value;
+    document.getElementById("colorStartRad").textContent = this.value;
+  } else if (`--${this.name}` === '--colorEnd') {
+    document.getElementById("colorEnd").textContent = this.value;
+    document.getElementById("colorEndRad").textContent = this.value;
   }
 }
 
 function directionUpdate() {
+  body.classList.remove("radial-grad");
+  linearCode.classList.remove("visually-hidden");
+  radialCode.classList.add("visually-hidden");
   document.documentElement.style.setProperty(`--${this.name}`, this.value);
-  document.getElementById("direction").innerHTML = this.value;
+  document.getElementById("direction").textContent = this.value;
 }
+
+radial.addEventListener("click", function() {
+  body.classList.add("radial-grad");
+  linearCode.classList.add("visually-hidden");
+  radialCode.classList.remove("visually-hidden");
+})
 
 colorStart.addEventListener('change', handleUpdate);
 colorEnd.addEventListener('change', handleUpdate);
