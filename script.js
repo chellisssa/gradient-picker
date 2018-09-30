c0e18915b82d59ce0202cf0bb2fe0343c927da31
chellisssa <chellisssa@gmail.com>
2018-09-30 21:22:50 +0300

    Added change of the colors using text inputs

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 var colorStart = document.querySelector("#start");
 var colorEnd = document.querySelector("#end");
-var toTop = document.querySelector(".top");
-var toTopRight = document.querySelector(".top-right");
-var toRight = document.querySelector(".right");
-var toBottomRight = document.querySelector(".bottom-right");
-var toBottom = document.querySelector(".bottom");
-var toBottomLeft = document.querySelector(".bottom-left");
-var toLeft = document.querySelector(".left");
-var toTopLeft = document.querySelector(".top-left");
 var output = document.querySelector(".output");
 var radial = document.querySelector(".radial");
 var body = document.querySelector("body");
 var radialCode = document.querySelector(".radial-code");
 var linearCode = document.querySelector(".linear-code");
+var changeStart = document.querySelector(".change-start");
+var changeEnd = document.querySelector(".change-end");
 
 // DEFAULT VALUES
 document.getElementById("colorStart").innerHTML = "#ff80ff";
 document.getElementById("colorStartRad").innerHTML = "#ff80ff";
 document.getElementById("colorEnd").innerHTML = "#ffff80";
 document.getElementById("colorEndRad").innerHTML = "#ffff80";
+changeStart.value = "#ff80ff";
+changeEnd.value = "#ffff80";
 document.getElementById("direction").innerHTML = "to top right";
 
 
@@ -27,18 +23,20 @@ function handleUpdate() {
   if (`--${this.name}` === '--colorStart') {
     document.getElementById("colorStart").textContent = this.value;
     document.getElementById("colorStartRad").textContent = this.value;
+    changeStart.value = this.value;
   } else if (`--${this.name}` === '--colorEnd') {
     document.getElementById("colorEnd").textContent = this.value;
     document.getElementById("colorEndRad").textContent = this.value;
+    changeEnd.value = this.value;
   }
 }
 
-function directionUpdate() {
+function directionUpdate(value) {
   body.classList.remove("radial-grad");
   linearCode.classList.remove("visually-hidden");
   radialCode.classList.add("visually-hidden");
-  document.documentElement.style.setProperty(`--${this.name}`, this.value);
-  document.getElementById("direction").textContent = this.value;
+  document.documentElement.style.setProperty(`--direction`, value);
+  document.getElementById("direction").textContent = value;
 }
 
 radial.addEventListener("click", function() {
@@ -49,12 +47,5 @@ radial.addEventListener("click", function() {
 
 colorStart.addEventListener('change', handleUpdate);
 colorEnd.addEventListener('change', handleUpdate);
-
-toTop.addEventListener("click", directionUpdate);
-toTopRight.addEventListener("click", directionUpdate);
-toRight.addEventListener("click", directionUpdate);
-toBottomRight.addEventListener("click", directionUpdate);
-toBottom.addEventListener("click", directionUpdate);
-toBottomLeft.addEventListener("click", directionUpdate);
-toLeft.addEventListener("click", directionUpdate);
-toTopLeft.addEventListener("click", directionUpdate);
+changeStart.addEventListener('change', handleUpdate);
+changeEnd.addEventListener('change', handleUpdate)
